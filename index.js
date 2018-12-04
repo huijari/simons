22d821ac9607e58338bb89c59591902f41c05a0d
Alexandre Cesar <acmorcego@gmail.com>
2018-12-04 11:16:27 -0200

    add prettier

--- index.js
+++ index.js
@@ -5,27 +5,27 @@ const run = require('./src/run')
 
 const telegram = new Telegram(process.env.BOT_KEY)
 function reply(chat, message, text) {
-  telegram.sendMessage(chat, text, {
-    reply_to_message_id: message
-  })
+	telegram.sendMessage(chat, text, {
+		reply_to_message_id: message
+	})
 }
 
 async function json(request) {
-  const body = await getRawBody(request, { encoding: true })
-  return JSON.parse(body)
+	const body = await getRawBody(request, { encoding: true })
+	return JSON.parse(body)
 }
 
 async function handler(request, response) {
-  const { message } = await json(request)
-  if (message && message.text) {
-    const values = await run(message)
-    if (values.length !== 0) {
-      const responses = values.map(([from, to]) => `${from} ↝ ${to}`)
-      reply(message.chat.id, message.message_id, responses.join('\n'))
-    }
-  }
+	const { message } = await json(request)
+	if (message && message.text) {
+		const values = await run(message)
+		if (values.length !== 0) {
+			const responses = values.map(([from, to]) => `${from} ↝ ${to}`)
+			reply(message.chat.id, message.message_id, responses.join('\n'))
+		}
+	}
 
-  response.end()
+	response.end()
 }
 
 module.exports = handler
--- src/run.js
+++ src/run.js
@@ -1,10 +1,10 @@
 const dolar = require('./dolar')
 
 async function run({ text }) {
-  const parsers = [dolar]
-  const promisedResults = parsers.map(async parser => parser(text))
-  const results = await Promise.all(promisedResults) 
-  return results.flat()
+	const parsers = [dolar]
+	const promisedResults = parsers.map(async parser => parser(text))
+	const results = await Promise.all(promisedResults)
+	return results.flat()
 }
 
 module.exports = run
