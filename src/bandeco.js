8aadc361e86dd9811d98d72fcb39c87fc4c683fc
Alexandre Cesar <acmorcego@gmail.com>
2019-05-02 12:40:32 -0300

    add bandeco parser

---
+++ src/bandeco.js
@@ -0,0 +1,36 @@
+const { post } = require('got')
+
+async function getCardapios(names) {
+	const url = 'http://minha.fump.ufmg.br/fumpWs/cardapioRU'
+	const token = process.env.FUMP_TOKEN
+	const { body } = await post(url, {
+		json: true,
+		headers: {
+			Authorization: `Basic ${token}`
+		}
+	})
+	return body.filter(({ restaurante }) => names.includes(restaurante))
+}
+
+function restaurants(param) {
+	const names = ['RU Setorial I', 'RU Setorial II']
+	if (param === '') return names
+	if (param === 'i') return [names[0]]
+	if (param === 'ii') return [names[1]]
+	return []
+}
+
+function view({ restaurante, tipoRefeicao, pratos }) {
+	return [`${restaurante} (${tipoRefeicao})`, ['', ...pratos, ''].join('\n')]
+}
+
+async function parse(text) {
+	const pattern = /^\/ru(i*)/
+	const match = pattern.exec(text)
+	if (match === null) return []
+
+	const cardapios = await getCardapios(restaurants(match[1]))
+	return cardapios.map(view)
+}
+
+module.exports = parse
--- src/run.js
+++ src/run.js
@@ -1,9 +1,10 @@
+const bandeco = require('./bandeco')
 const calendar = require('./calendar')
 const dolar = require('./dolar')
 const euro = require('./euro')
 
 async function run({ text }) {
-	const parsers = [calendar, dolar, euro]
+	const parsers = [bandeco, calendar, dolar, euro]
 	const promisedResults = parsers.map(async parser => parser(text))
 	const results = await Promise.all(promisedResults)
 	return results.flat()
