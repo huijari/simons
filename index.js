f56381e4dbbc8f8de7e3b9481a7301d04b7d61c9
Alexandre Cesar <acmorcego@gmail.com>
2019-12-07 21:42:17 -0300

    return responses when in debug mode

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const { json } = require('micro')
 
 const run = require('./src/run')
 
+const debug = process.env.DEBUG !== undefined
+
 const telegram = new Telegram(process.env.BOT_KEY)
-function reply(chat, message, text) {
-	if (process.env.DEBUG !== undefined) console.log({ text })
-	else
-		telegram.sendMessage(chat, text, {
-			reply_to_message_id: message,
-			parse_mode: 'HTML'
-		})
+function reply({ message_id, chat }, text) {
+	telegram.sendMessage(chat.id, text, {
+		reply_to_message_id: message_id,
+		parse_mode: 'HTML'
+	})
 }
 
 async function handler(request, response) {
@@ -21,8 +21,11 @@ async function handler(request, response) {
 	if (message && message.text) {
 		const values = await run(message)
 		if (values.length !== 0) {
-			const responses = values.map(([from, to]) => `${from} ↝ ${to}`)
-			reply(message.chat.id, message.message_id, responses.join('\n'))
+			const responses = values.map(([from, to]) => `${from} ↝ ${to}`).join('\n')
+			if (debug) {
+				console.log({ responses })
+				return responses
+			} else reply(message, responses)
 		}
 	}
 
