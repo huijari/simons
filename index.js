9fee19a0c2f656ea9bb713c2b7ebc5d97f256c45
Alexandre Cesar <acmorcego@gmail.com>
2020-06-02 11:36:48 -0300

    implement events

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Telegram = require('telegraf/telegram')
 const { json } = require('micro')
 
 const run = require('./src/run')
+const events = require('./src/events')
 
 const debug = process.env.DEBUG !== undefined
 
@@ -15,10 +16,10 @@ function reply({ message_id, chat }, text) {
 	})
 }
 
-async function handler(request, response) {
+async function handler(request) {
 	const { message } = await json(request)
 
-	if (message && message.text) {
+	if (message?.text !== undefined) {
 		const values = await run(message)
 		if (values.length !== 0) {
 			const responses = values.map(([from, to]) => `${from} ↝ ${to}`).join('\n')
@@ -27,6 +28,11 @@ async function handler(request, response) {
 				return responses
 			} else reply(message, responses)
 		}
+	}
+
+	if (message?.event !== undefined) {
+		const event = events[message.event]
+		if (event !== undefined) reply(message, event)
 	}
 
 	return ''
---
+++ src/events.js
@@ -0,0 +1,7 @@
+const events = {
+	water: 'Reminder ↝ drink water',
+	posture: 'Reminder ↝ posture',
+	lunch: 'Reminder ↝ stop for lunch'
+}
+
+module.exports = events
