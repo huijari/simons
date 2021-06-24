3b6fe944ad8a51c21e39007f1d0b7606c4ca359c
Alexandre Cesar <acmorcego@gmail.com>
2021-06-24 10:46:02 -0300

    fix tracking regex and text encoding

--- src/correios.js
+++ src/correios.js
@@ -1,26 +1,30 @@
 const got = require('got')
 const cheerio = require('cheerio')
+const { decode } = require('iconv-lite')
 
 async function getStatus(objetos) {
 	const url =
 		'https://www2.correios.com.br/sistemas/rastreamento/ctrl/ctrlRastreamento.cfm'
 	try {
-		const text = await got.post(url, { form: { objetos } }).text()
+		const buffer = await got.post(url, { form: { objetos } }).buffer()
+		const text = decode(buffer, 'latin1')
 		const dom = cheerio.load(text)
 		const status = dom('.sroLbEvent').first().text().trim()
-		return status
-			.replace('\n', ',')
-			.replaceAll('\t', '')
-			.replaceAll('\n', ' ')
-			.replaceAll(/\s+/g, ' ')
+		return (
+			status
+				.replace('\n', ',')
+				.replaceAll('\t', '')
+				.replaceAll('\n', ' ')
+				.replaceAll(/\s+/g, ' ') || 'not found'
+		)
 	} catch (e) {
 		console.log(e)
-		return 'correios didn\'t respond'
+		return "correios didn't respond"
 	}
 }
 
 async function parse(text) {
-	const pattern = /\w{2}\d{9}\w{2}/
+	const pattern = /\p{L}{2}\d{9}\p{L}{2}/u
 	const match = pattern.exec(text)
 	if (match === null) return []
 
